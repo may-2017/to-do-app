1497a2112c6affaa2f987358449b5398edabfd59
songmeizhu <smzppp@gmail.com>
2018-08-13 23:36:27 -0700

    simpletodo delete

--- app.js
+++ app.js
@@ -2,6 +2,9 @@ function onReady() {
   const addToDoForm = document.getElementById('addToDoForm');
   const newToDoText = document.getElementById('newToDoText');
   const toDoList = document.getElementById('toDoList');
+
+  var todos = new Array();
+
   addToDoForm.addEventListener('submit', () => {
     event.preventDefault();
     // get the text
@@ -12,23 +15,51 @@ function onReady() {
 
     // create a new input
     let checkbox = document.createElement('input');
+    let button = document.createElement('button');
+
 
     // set the input's type to checkbox
     checkbox.type = "checkbox";
 
     // set the title
     newLi.textContent = title;
+    newLi.id = todos.length;
+    todos.push(newLi);
 
     // attach the checkbox to the li
     newLi.appendChild(checkbox);
+    newLi.appendChild(button);
 
     // attach the li to the ul
     toDoList.appendChild(newLi);
 
     //empty the input
     newToDoText.value = '';
+    button.textContent = 'Delete';
+    button.type = 'delete';
+    button.onclick = deleteItem;
   });
- }
+
+  function deleteItem(e) {
+    var id = e.target.parentElement.id;
+    console.log("delete an item: " + id);
+    // find and remove the item in the array
+
+
+    for (var i = 0; i < todos.length; i++) {
+      if (todos[i].id == id) {
+        var li = e.target.parentElement;
+        todos.splice(i, 1);
+        break;
+      }
+    }
+
+    // find and remove the item in the page
+    var ul = document.getElementById("toDoList");
+    var li = e.target.parentElement;
+    ul.removeChild(li);
+  }
+}
 window.onload = function() {
   onReady();
 };
